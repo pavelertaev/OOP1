1ea8e62640abd8c16e8e9897176f1ec437a98679
паша <pashka_ertaev@mail.ru>
2022-10-22 11:17:22 +0300

    Задание выполнено ожидает проверки

--- src/People.java
+++ src/People.java
@@ -1,7 +1,7 @@
 public class People {
     String name;
-    int dateOfBirth;
-    String city;
+    private int dateOfBirth;
+    private String city;
     String job;
 
     People(String name , int dateOfBirth , String city,String job){
@@ -32,4 +32,26 @@ public class People {
         System.out.println("Привет! Меня зовут " + name + " . Я из города " + city + ". Я родился в " + dateOfBirth + " году. Я работаю на должности " + job + ".Будем знакомы!" );
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        if(city != null && city.isEmpty() ){
+            this.city=city;
+        }else
+            this.city="Информация не указана";
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        if(dateOfBirth>0 && dateOfBirth != ' '){
+            this.dateOfBirth=dateOfBirth;
+        }else
+            this.dateOfBirth=0;
+
+    }
 }
